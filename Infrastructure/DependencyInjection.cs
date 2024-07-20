4e71c654df5a6f01f36cbc816c820ec581c007b0
b.a.n <barmin.andrey.nnov@gmail.com>
2024-07-20 12:30:27 +0300

    cleanup

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,23 +1,10 @@
-﻿using FluentValidation.AspNetCore;
-using Infrastructure.EntityFrameworkCore;
+﻿using Infrastructure.EntityFrameworkCore;
 using Interfaces.DbContexts;
 
 public static class DependencyInjection__ForumsInfrastructure
 {
-    static void Configure(this IServiceCollection Services)
-    {
-        Services.AddLogging();
-
-        Services.AddFluentValidationAutoValidation();
-        Services.AddFluentValidationClientsideAdapters();
-        Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-    }
-
-
     public static void AddInfrastructureUseSqlServer(this IServiceCollection Services, string ConnectionString)
     {
-        Services.Configure();
-
         Services.AddDbContext<AppDbContext>(options =>
         {
             options.UseSqlServer(ConnectionString);
@@ -25,13 +12,9 @@ public static class DependencyInjection__ForumsInfrastructure
         Services.AddScoped<IAppDbContext, AppDbContext>();
     }
 
-
-
     public static void AddInfrastructureUseSqlite(this IServiceCollection Services
         , string ConnectionString= $"FileName=AppDbContext.db")
 	{
-        Services.Configure();
-
         Services.AddDbContext<AppDbContext>(options =>
 		{
 			options.UseSqlite(ConnectionString);
@@ -42,9 +25,6 @@ public static class DependencyInjection__ForumsInfrastructure
 
 	public static void AddInfrastructureUseInMemoryDatabase(this IServiceCollection Services, string DataBaseName)
 	{
-        Services.Configure();
-
-
         Services.AddDbContext<AppDbContext>(options =>
 		{
 			options.UseInMemoryDatabase(DataBaseName);
@@ -57,8 +37,6 @@ public static class DependencyInjection__ForumsInfrastructure
     public static void AddInfrastructureUseMySql(this IServiceCollection Services, 
         string ConnectionString = "Server=localhost;Uid=mysql;Pwd=mysql;Database=OrderingApi;")
 	{
-        Services.Configure();
-
         Services.AddDbContext<AppDbContext>(options =>
 		{
 			options.UseMySql(ConnectionString, ServerVersion.AutoDetect(ConnectionString));
